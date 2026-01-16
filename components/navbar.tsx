6bdd49999e7b6eb1a8b1c72810e4a458d05404f4
dinal <150317435+kordinaldepriansya@users.noreply.github.com>
2026-01-16 19:32:02 +0700

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,3 +2,5 @@ import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import db from "@/lib/db";
+
+import StoreSwitcher from "@/components/store-switcher";
