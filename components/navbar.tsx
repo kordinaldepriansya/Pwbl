f17f2cdb80fd63fc7bbc8c5be3761db40da31033
dinal <150317435+kordinaldepriansya@users.noreply.github.com>
2026-01-16 19:33:09 +0700

    Add Separator import to navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,3 +7,4 @@ import StoreSwitcher from "@/components/store-switcher";
 import { MobileNav } from "@/components/mobile-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { CommandSearch } from "@/components/command-search";
+import { Separator } from "@/components/ui/separator";
