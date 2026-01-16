50f92c6641e994ac44e1a347f40cbd5f8bd4abc4
dinal <150317435+kordinaldepriansya@users.noreply.github.com>
2026-01-16 19:32:52 +0700

    Add CommandSearch import to navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,3 +6,4 @@ import db from "@/lib/db";
 import StoreSwitcher from "@/components/store-switcher";
 import { MobileNav } from "@/components/mobile-nav";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { CommandSearch } from "@/components/command-search";
