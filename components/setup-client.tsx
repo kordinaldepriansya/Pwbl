a32b03524d0adb5caa6288609ed020539bae4cf7
dinal <150317435+kordinaldepriansya@users.noreply.github.com>
2026-01-16 19:37:46 +0700

    Update setup-client.tsx

--- components/setup-client.tsx
+++ components/setup-client.tsx
@@ -7,11 +7,3 @@ export const SetupClient = () => {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
-  useEffect(() => {
-    if (!isOpen) {
-      onOpen();
-    }
-  }, [isOpen, onOpen]);
-
-  return null;
-};
